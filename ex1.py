fa2313153a4bbfe5bfc5b237c5fc8a243b2bb750
maxwebberley <webberley.max@gmail.com>
2019-03-16 16:56:12 +0000

    fn tracker

--- ex1.py
+++
@@ -1,7 +0,0 @@
-print "Hello World!"
-print "Helllo Again"
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much wather you 'not'."
-print 'I "said" do not touch this.'
